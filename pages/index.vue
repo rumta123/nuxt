6c674028386b2f65a0da1e42a9c71a8929995267
rumta123 <rumta@yandex.ru>
2022-12-10 03:16:09 +0300

    new add menu

--- pages/index.vue
+++ pages/index.vue
@@ -27,43 +27,7 @@
               From: "opacity-100 scale-100"
               To: "opacity-0 scale-95"
           -->
-            <div class="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden">
-              <div class="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
-                <div class="flex items-center justify-between px-5 pt-4">
-                  <div>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt="">
-                  </div>
-                  <div class="-mr-2">
-                    <button type="button"
-                      class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                      <span class="sr-only">Close main menu</span>
-                      <!-- Heroicon name: outline/x-mark -->
-                      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <div class="space-y-1 px-2 pt-2 pb-3">
-                  <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">Product</a>
 
-                  <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">Features</a>
-
-                  <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">Marketplace</a>
-
-                  <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">Company</a>
-                </div>
-                <a href="#"
-                  class="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100">Log
-                  in</a>
-              </div>
-            </div>
           </div>
 
           <main class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
@@ -130,7 +94,11 @@ export default {
   }
 };
 </script>
-
+<style>
+.ease-in-out {
+  width: 100vw;
+}
+</style>
 <style scoped>
 .h-56 {
   height: 100%;
